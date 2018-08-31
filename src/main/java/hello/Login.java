c19bfd3ee36a6c248c3ec1363412c250ca1658df
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-08-31 00:18:35 -0300

    Login endpoint added

---
+++ src/main/java/hello/Login.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Login {
+
+    private final String content;
+
+    public Login(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/hello/LoginController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class LoginController {
+    @PostMapping("/login")
+    public Login message() {
+        return new Login("Logged in!");
+    }
+}
